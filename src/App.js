c8d76ac782b16de1bc02e1de8ae6057b5b810f1a
Sagar247776 <sn9758067077@gmail.com>
2022-01-25 02:32:02 +0530

    Fix:Routing

--- src/App.js
+++ src/App.js
@@ -1,55 +1,50 @@
-import './App.css';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import React, { useState } from 'react';
-import Alert from './components/Alert';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
 
 function App() {
-  const [mode, setMode] = useState('light');
-  const [alert, setAlert] = useState(null)
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
-    setTimeout(() =>{
+      type: type,
+    });
+    setTimeout(() => {
       setAlert(null);
     }, 2000);
-      
-  }
+  };
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#212121';
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#212121";
       showAlert(" Dark mode has been Enabled", "light");
-      document.title = 'Text Champ - Dark'
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+      document.title = "Text Champ - Dark";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert(" Light mode has been Enabled", "dark");
-      document.title = 'Text Champ - Light'
-
+      document.title = "Text Champ - Light";
     }
-  }
+  };
   return (
     <>
-    <div>
-      <Navbar mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
+      <div>
+        <Navbar mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
       </div>
       <div className="container my-3">
-      <TextForm showAlert={showAlert} heading="Enter the Text to Analyze" mode={mode}/>
-      
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the Text to Analyze"
+          mode={mode}
+        />
       </div>
-      <div>
-        {/* <About/>     */} 
-        
-      </div>
+      <div>{/* <About/>     */}</div>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,65 +1,109 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const handleUpClick = ()=>{
-        let newText =text.toUpperCase();
-        setText(newText)
-        props.showAlert("   Converted to Uppercase!" , "primary")
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("   Converted to Uppercase!", "primary");
+  };
 
-    }
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("   Converted to Lowercase!", "primary");
+  };
 
-    const handleLoClick = ()=>{
-            let newText =text.toLowerCase();
-            setText(newText)
-            props.showAlert("   Converted to Lowercase!" , "primary")
-    }
-    
-    const handleCopy = () =>{
-        var text = document . getElementById("myBox") ;
-        text. select( ) ;
-        navigator . clipboard. writeText(text . value) ;
-        props.showAlert("   Text Copy Successfully" , "success")
-    }
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("   Text Copy Successfully", "success");
+  };
 
-    const handleExtraSpaces = () => {
-        let newText = text . split(/ [ ]+/);
-        setText (newText. join(" "))
-        props.showAlert("   Text Extra Space is Removed Successfully" , "warning")
-    }
+  const handleExtraSpaces = () => {
+    let newText = text.split(/ [ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("   Text Extra Space is Removed Successfully", "warning");
+  };
 
-
-    const handleClearClick = ()=>{
-        let newText ='';
-        setText(newText)
-        props.showAlert("   Text is Clear Successfully" , "danger")
-}
-    const handleOnChange = (event)=>{
-        setText(event.target.value);
-    }
-    const[text, setText] = useState('');
-    return (
-        <>
-        <div className="container" style={{color: props.mode==='dark'?'white':'#212121'}}>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#212121' }} id="myBox" rows="8"></textarea>
-            </div>
-            <button type="button" className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button type="button" className="btn btn-danger mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button type="button" className="btn btn-warning mx-2" onClick={handleExtraSpaces}>Remove Space</button>  
-            <button type="button" className="btn btn-success mx-2" onClick={handleCopy}>Copy Text</button> 
-            <button type="button" className="btn btn-dark mx-2" onClick={handleClearClick}>Clear Text</button>            
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("   Text is Clear Successfully", "danger");
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#212121" }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#212121",
+            }}
+            id="myBox"
+            rows="8"
+          ></textarea>
         </div>
+        <button
+          type="button"
+          className="btn btn-primary mx-2"
+          onClick={handleUpClick}
+        >
+          Convert to Uppercase
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger mx-2"
+          onClick={handleLoClick}
+        >
+          Convert to Lowercase
+        </button>
+        <button
+          type="button"
+          className="btn btn-warning mx-2"
+          onClick={handleExtraSpaces}
+        >
+          Remove Space
+        </button>
+        <button
+          type="button"
+          className="btn btn-success mx-2"
+          onClick={handleCopy}
+        >
+          Copy Text
+        </button>
+        <button
+          type="button"
+          className="btn btn-dark mx-2"
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
+      </div>
 
-        <div className="container m-3" style={{color: props.mode==='dark'?'white':'#212121'}}>
-            <h2>Your text summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes Read</p>
-            <h2>Preview</h2>
-            <p>{text.length>0?text:"Write Your Text to Preview"}</p>
-        </div>
-
-        </>
-    )
+      <div
+        className="container m-3"
+        style={{ color: props.mode === "dark" ? "white" : "#212121" }}
+      >
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Write Your Text to Preview"}</p>
+      </div>
+    </>
+  );
 }
