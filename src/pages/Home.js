9c399b7152681c2051799ec3d9734c8c98f9fd7c
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-02-12 17:07:56 +0530

    Text Champ 2.0
    New Update Fixing all the problems in 1.0

---
+++ src/pages/Home.js
@@ -0,0 +1,52 @@
+import "../App.css";
+import Navbar from "../components/Navbar";
+import TextForm from "../components/TextForm";
+import React, { useState } from "react";
+import Alert from "../components/Alert";
+
+function Home() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#212121";
+      showAlert(" Dark mode has been Enabled", "light");
+      document.title = "Text Champ - Dark";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert(" Light mode has been Enabled", "dark");
+      document.title = "Text Champ - Light";
+    }
+  };
+  return (
+    <>
+      <div>
+        <Navbar mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+      </div>
+      <div className="container my-3">
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the Text to Analyze"
+          mode={mode}
+        />
+      </div>
+      <div>{/* <About/>     */}</div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/Home2.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home2() {
+  return (
+    <div>
+        <div>
+        <div>Logo</div>
+        <div>
+        <a>Home</a>
+        <a>About</a>
+      </div>
+        </div>
+        <div>
+            Mode
+        </div>
+    </div>
+  );
+}
+
+export default Home2;
