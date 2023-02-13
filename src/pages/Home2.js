bdf74038d868a3faa66d84d1395c57f366a2c302
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-02-13 15:06:45 +0530

    btn work done and css work is done

--- src/pages/Home2.js
+++ src/pages/Home2.js
@@ -1,34 +1,49 @@
 import React from "react";
-import { Box,  useTheme,Button  } from "@mui/material";
-import {  tokens } from "../theme";
-import { red } from '@mui/material/colors';
-
+import { Box, useTheme } from "@mui/material";
+import { tokens } from "../theme";
 import TextareaAutosize from "@mui/base/TextareaAutosize";
-import SendIcon from '@mui/icons-material/Send';
+import FontDownloadIcon from '@mui/icons-material/FontDownload';
+import FontDownloadOffIcon from '@mui/icons-material/FontDownloadOff';
+import SpaceBarIcon from '@mui/icons-material/SpaceBar';
+import CopyAllIcon from '@mui/icons-material/CopyAll';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 function Home2() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
     <div className="home">
-      
-      <Box sx={{display:"flex", flexFlow:"column", alignItems:"center"}}>
-      <Box>
-      <h1>Enter the Text to Analyze</h1>
-        <TextareaAutosize
-          minRows={15}
-          maxRows={18}
-          aria-label="maximum height"
-          placeholder="enter your text here"
-          style={{ width: "80vw", backgroundColor: colors.primary[400], color: colors.text[400],padding: "10px" }}
-        />
-      </Box>
-      <Box>
-      <Button variant="contained" color="success" endIcon={<SendIcon />}>
-        UpperCase
-      </Button>
 
-      </Box>
-      </Box>
+        <Box>
+          <h1>Enter the Text to Analyze</h1>
+          <TextareaAutosize
+          className="area-size"
+            minRows={15}
+            maxRows={18}
+            aria-label="maximum height"
+            placeholder="enter your text here"
+            style={{
+              backgroundColor: colors.primary[400],
+              color: colors.text[400],
+              padding: "10px",
+            }}
+          />
+        </Box>
+        <Box>
+          <div className="btns">
+            <button className="custom-btn btn-1"><span><FontDownloadIcon/></span> Uppercase</button>
+            <button className="custom-btn btn-2"><span><FontDownloadOffIcon/></span> Lowercase</button>
+            <button className="custom-btn btn-3"><span><SpaceBarIcon/></span> Remove Space</button>
+            <button className="custom-btn btn-4"><span><CopyAllIcon/></span> Copy Text</button>
+            <button className="custom-btn btn-5"><span><DeleteIcon/></span> Clear Text</button>
+          </div>
+        </Box>
+      <div className="text-summary">
+        <h2>Your text summary</h2>
+        <h6>1 words and 0 characters
+          <br/>
+          0.008 Minutes Read</h6>
+      </div>
     </div>
   );
 }
